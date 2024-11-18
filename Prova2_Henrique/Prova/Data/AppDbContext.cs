4b7502623e01f7ac6568d5c9444163eed7b71818
Giandonn <lucas_raineri04@hotmail.com>
2024-11-18 18:26:29 -0300

    add produto e marca ao dbContext

--- Prova2_Henrique/Prova/Data/AppDbContext.cs
+++ Prova2_Henrique/Prova/Data/AppDbContext.cs
@@ -1,29 +1,41 @@
 using Microsoft.EntityFrameworkCore;
 using Prova.Models;
 
-namespace Prova.Data
-{
-    public class AppDbContext : DbContext
-    {
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
+namespace Prova.Data {
+	public class AppDbContext : DbContext {
+		public DbSet<Usuario> Usuarios { get; set; }
+		public DbSet<Endereco> Enderecos { get; set; }
+		public DbSet<Marca> Marcas { get; set; } 
+		public DbSet<Produto> Produtos { get; set; } 
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Endereco>()
-                .HasOne(e => e.Usuario)
-                .WithMany(u => u.Enderecos)
-                .HasForeignKey(e => e.UsuarioId);
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			modelBuilder.Entity<Endereco>()
+				.HasOne(e => e.Usuario)
+				.WithMany(u => u.Enderecos)
+				.HasForeignKey(e => e.UsuarioId);
 
-            modelBuilder.Entity<Endereco>()
-                .Property(e => e.Id)
-                .ValueGeneratedOnAdd();
+			modelBuilder.Entity<Endereco>()
+				.Property(e => e.Id)
+				.ValueGeneratedOnAdd();
 
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+			modelBuilder.Entity<Produto>()
+				.HasOne<Marca>()
+				.WithMany() 
+				.HasForeignKey(p => p.IdMarca) 
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Marca>()
+				.Property(m => m.Id)
+				.ValueGeneratedOnAdd();
+
+			modelBuilder.Entity<Produto>()
+				.Property(p => p.Id)
+				.ValueGeneratedOnAdd();
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
 }
