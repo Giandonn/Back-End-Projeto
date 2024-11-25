d85336ac011e93ab63618305962c84611c13185c
Giandonn <lucas_raineri04@hotmail.com>
2024-11-25 20:48:50 -0300

    aaa

--- Prova2_Henrique/Testes/SystemFlowTests.cs
+++ Prova2_Henrique/Testes/SystemFlowTests.cs
@@ -11,7 +11,7 @@ namespace Testes
     public class SystemFlowTests : IDisposable
     {
         private readonly IWebDriver driver;
-        private readonly string connectionString = "Server=localhost;Database=YourDatabase;User Id=YourUser;Password=YourPassword;";
+        private readonly string connectionString = "Server=(localdb)\\mssqllocaldb;Database=UsuarioDB;ConnectRetryCount=0;";
 
         public SystemFlowTests()
         {
@@ -21,38 +21,62 @@ namespace Testes
         [Fact]
         public void CompleteUserFlowTest()
         {
-            string testEmail = $"emaildetest@gmail.com";
+            string testEmail = "emaildetest@gmail.com";
             string testPassword = "Senha123!";
-            string newName = "Jorjinho do tenis";
+            string testNome = "Jorjinho";
+            string testSobrenome = "do tenis";
+            string testIdade = "2000-05-15"; 
+            string testTelefone = "11987654321"; 
+            string testCep = "12345678"; 
+            string testEstado = "São Paulo";
+            string testCidade = "São Paulo"; 
+            string newName = "Jorjinho"; 
 
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-            driver.Navigate().GoToUrl("http://localhost/register");
-            wait.Until(driver => driver.FindElement(By.Id("email"))).SendKeys(testEmail);
-            driver.FindElement(By.Id("password")).SendKeys(testPassword);
-            driver.FindElement(By.Id("confirmPassword")).SendKeys(testPassword);
-            driver.FindElement(By.Id("registerButton")).Click();
+            driver.Navigate().GoToUrl("http://127.0.0.1:5502/cadastro.html");
 
-            driver.Navigate().GoToUrl("http://localhost/login");
-            wait.Until(driver => driver.FindElement(By.Id("email"))).SendKeys(testEmail);
-            driver.FindElement(By.Id("password")).SendKeys(testPassword);
-            driver.FindElement(By.Id("loginButton")).Click();
+            wait.Until(driver => driver.FindElement(By.Id("inputNome"))).SendKeys(testNome);
+            wait.Until(driver => driver.FindElement(By.Id("inputSobrenome"))).SendKeys(testSobrenome);
+            wait.Until(driver => driver.FindElement(By.Id("inputIdade"))).SendKeys(testIdade);
+            wait.Until(driver => driver.FindElement(By.Id("inputTelefone"))).SendKeys(testTelefone);
+            wait.Until(driver => driver.FindElement(By.Id("inputEmail"))).SendKeys(testEmail);
+            wait.Until(driver => driver.FindElement(By.Id("inputPassword"))).SendKeys(testPassword);
+            wait.Until(driver => driver.FindElement(By.Id("inputConfirmPassword"))).SendKeys(testPassword);
+            wait.Until(driver => driver.FindElement(By.Id("inputCep"))).SendKeys(testCep);
 
-            driver.Navigate().GoToUrl("http://localhost/profile");
-            wait.Until(driver => driver.FindElement(By.Id("name"))).Clear();
-            driver.FindElement(By.Id("name")).SendKeys(newName);
-            driver.FindElement(By.Id("saveProfileButton")).Click();
+            wait.Until(driver => driver.FindElement(By.Id("inputEstado"))).SendKeys(testEstado);
+            wait.Until(driver => driver.FindElement(By.Id("inputCidade"))).SendKeys(testCidade);
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+
+            driver.Navigate().GoToUrl("http://127.0.0.1:5502/login.html");
+
+            wait.Until(driver => driver.FindElement(By.Id("inputEmail"))).SendKeys(testEmail);
+            wait.Until(driver => driver.FindElement(By.Id("inputPassword"))).SendKeys(testPassword);
+            driver.FindElement(By.Id("loginbtn")).Click();
+
+            try
             {
-                connection.Open();
-                SqlCommand command = new SqlCommand("SELECT Name FROM Users WHERE Email = @Email", connection);
-                command.Parameters.AddWithValue("@Email", testEmail);
-                string dbName = command.ExecuteScalar()?.ToString();
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT Email FROM Usuarios WHERE Email = @Email", connection);
+                    command.Parameters.AddWithValue("@Email", testEmail);
+                    string dbName = command.ExecuteScalar()?.ToString();
 
-                Assert.Equal(newName, dbName);
+                    Assert.Equal(testEmail, dbName);
+                }
             }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Erro ao executar a consulta: {ex.Message}");
+                throw;  
+            }
+
         }
+
 
         public void Dispose()
         {
