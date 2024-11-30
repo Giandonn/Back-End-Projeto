175ef99f41ae789125c659770b20720105d98052
mattmachad <matheus.machado@pedbot.com.br>
2024-11-29 21:26:57 -0300

    fixing conflicts

---
+++ Prova2_Henrique/Prova/Infrastructure/Repositories/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Prova.Models;
+
+namespace Prova.Infrastructure.Repositories.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Endereco>()
+                .HasOne(e => e.Usuario)
+                .WithMany(u => u.Enderecos)
+                .HasForeignKey(e => e.UsuarioId);
+
+            modelBuilder.Entity<Endereco>()
+                .Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Produto>()
+                .HasOne<Marca>()
+                .WithMany()
+                .HasForeignKey(p => p.IdMarca)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Marca>()
+                .Property(m => m.Id)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Id)
+                .ValueGeneratedOnAdd();
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Prova2_Henrique/Prova/Infrastructure/Repositories/Data/Migrations/20241023181253_InitialCreate.Designer.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Prova.Infrastructure.Repositories.Data;
+
+#nullable disable
+
+namespace Prova.Migrations
+{
+    [DbContext(typeof(AppDbContext))]
+    [Migration("20241023181253_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "8.0.10")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128);
+
+            SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("Prova.Models.Endereco", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Cep")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Cidade")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Estado")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("UsuarioId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UsuarioId");
+
+                    b.ToTable("Enderecos");
+                });
+
+            modelBuilder.Entity("Prova.Models.Usuario", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Senha")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Sobrenome")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Telefone")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Usuarios");
+                });
+
+            modelBuilder.Entity("Prova.Models.Endereco", b =>
+                {
+                    b.HasOne("Prova.Models.Usuario", "Usuario")
+                        .WithMany("Enderecos")
+                        .HasForeignKey("UsuarioId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Usuario");
+                });
+
+            modelBuilder.Entity("Prova.Models.Usuario", b =>
+                {
+                    b.Navigation("Enderecos");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}
